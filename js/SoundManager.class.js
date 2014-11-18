d20bda25d10f93554e08b5935e132f526c2d7b67
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2014-11-18 23:11:58 +0100

    play fonctionne mais ce n'est pas encore un node

--- js/SoundManager.class.js
+++ js/SoundManager.class.js
@@ -2,7 +2,6 @@
 (function() {
     "use strict";
     /*global AudioContext, webkitAudioContext, window*/
-    var analyzer;
     var self = null,                        // instance pour le context
         audioContext,                       // objet de l'api audioContext
         audioContextSupported = false,      // boolean permettant de savoir si le navigateur supporte web audio api
@@ -64,7 +63,7 @@
      * @param  {String} name Label du son que l'on veut charger
      * @param  {String} url  Adresse à laquelle se trouve le son
      */
-    SoundManager.prototype.loadSound = function( name, url ) {
+    SoundManager.prototype.loadSound = function(name, url) {
         var request = new XMLHttpRequest();
 
         request.open("GET", url, true);
@@ -75,9 +74,10 @@
         request.onload = function() {
             if (self.isAudioContextSupported) {
                 // au moment de la réponse on demande au contexte de décoder l'audio
-                audioContext.decodeAudioData( request.response,
+                audioContext.decodeAudioData(
+                    request.response,
                     function(buffer) {
-                        // on ra joute dans la variable globale le label, une version decodée de l'audio, ainsi qu'un tableau vide de nodes
+                        // on rajoute dans la variable globale le label, une version decodée de l'audio, ainsi qu'un tableau vide de nodes
                         sounds[name] = {
                             'buffer': buffer,
                             nodes: []
@@ -95,7 +95,63 @@
         request.send();
     };
 
-    if( window.SoundManager === undefined) {
+    SoundManager.prototype.play = function(name, timeBegin, timeEnding, loopFlag) {
+        if (sounds.hasOwnProperty(name)) {
+            if (self.isAudioContextSupported() === true) {
+                self.playSound(name, timeBegin, timeEnding, loopFlag);
+            }
+            else {
+                //
+            }
+        }
+        else {
+            console.log('???');
+            setTimeout(
+                function(){
+                    self.play(name, timeBegin, timeEnding, loopFlag);
+                },
+                500
+            );
+        }
+    };
+
+    SoundManager.prototype.playSound = function(soundName, timeBegin, timeEnding, loopFlag) {
+        var sourceNode = audioContext.createBufferSource(),
+            countItem,
+            i,
+            nodeTemp = [];
+
+        sourceNode.buffer = sounds[soundName].buffer;
+        
+        sourceNode.loop = (loopFlag === true);
+
+        if (timeEnding !== undefined) {
+            sourceNode.noteOff( audioContext.currentTime + timeEnding );
+        }
+
+        countItem = sounds[soundName].nodes.length;
+        if (countItem === 0) {
+            sourceNode.connect(audioContext.destination);
+        }
+        else {
+            for (i = 0; i < countItem; i = i+1) {
+                nodeTemp[i] = self.getNode(sounds[soundName].nodes[i][0],sounds[soundName].nodes[i][1]);
+
+                if (i === 0) {
+                    sourceNode.connect(nodeTemp[i]);
+                }
+                else {
+                    nodeTemp[i-1].connect(nodeTemp[i]);
+                }
+            }
+
+            nodeTemp[countItem-1].connect(audioContext.destination);
+        }
+
+        sourceNode.start(audioContext.currentTime + timeBegin);
+    };
+
+    if (window.SoundManager === undefined) {
         window.SoundManager = {};
     }
     window.SoundManager.SoundManager = SoundManager;
